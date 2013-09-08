0aff6ce5b6e682a8e5a4b965ef3a067c78d2dd4e
Daniel Lopes <dl.lopes@gmail.com>
2013-09-09 00:05:17 +0100

    widget implementation

--- src/main/javascript/siteSearch.js
+++ src/main/javascript/siteSearch.js
@@ -2,6 +2,7 @@
 YUI.add('site-search', function(Y) {
 
     var DLL = Y.namespace('DLL'),
+        Template = Y.Template.Micro,
         jobPoolCount = 4,
         doc = Y.config.doc,
         domain = doc.domain,
@@ -9,7 +10,8 @@ YUI.add('site-search', function(Y) {
         rxMatcher = /^([^:\/?#]+:)?(?:\/\/([^\/?#]*))?([^?#]+)?(\?[^#]*)?(#.*)?/,
         rxInvalidPath = /^[^?]/,
         SearchTask,
-        TaskPoolExecutor
+        TaskPoolExecutor,
+        Templates = {}
         ;
 
     DLL.SiteSearch = {
@@ -124,6 +126,50 @@ YUI.add('site-search', function(Y) {
 
     });
 
+    DLL.SiteSearch.Widget = Y.Base.create("SiteSearch.Widget", Y.Widget, [], {
+        renderUI: function () {
+            var node = this.get('contentBox'),
+                widget = Y.Node.create(Templates.widget())
+                ;
+
+            widget.one('.site-search.results').setStyles({'display': 'none'});
+            widget.appendTo(node);
+        },
+
+        bindUI: function () {
+            var node = this.get('contentBox'),
+                input = node.one('.site-search.query-input'),
+                results = node.one('.site-search.results')
+                ;
+
+            input.on('key', function () {
+                results.empty();
+                results.setStyles({
+                    'display': 'none'
+                });
+                SiteSearch.searchSite(input.get('value'), function (node) {
+                    results.setStyles({
+                        'display': 'block'
+                    });
+                    Y.Node.create('<a href="'+node.getAttribute('href')+'">'+node.get('text')+'</a><br>')
+                        .appendTo(results);
+                });
+            }, 'enter');
+        },
+
+        registerTemplate: function (name, templateCode) {
+            Templates[name] = Template.compile(templateCode);
+        }
+
+    });
+
+    Templates.widget = Template.compile(
+        '<div class="site-search container">' +
+            '<input type="text" class="site-search query-input"/>' +
+            '<div class="site-search results"></div>' +
+            '</div>'
+    );
+
 }, '1.0.0', {
-    requires: ['node', 'model', 'io-base']
+    requires: ['node', 'model', 'io-base', 'widget', 'template-micro', 'event']
 });
--- src/test/javascript/siteSearchTest.js
+++ src/test/javascript/siteSearchTest.js
@@ -52,4 +52,35 @@ describe('SiteSearch integration test', function() {
         }, "Anchors must be added to the page", 1000);
     });
 
+    it("test searching from widget with results", function () {
+        Y.Node.create('<div id="xpto"></div>').appendTo('body');
+
+        var widget = new SiteSearch.Widget(),
+            results;
+        widget.render('#xpto');
+
+        Y.one('#xpto .site-search.query-input').set('value', 'test');
+        Y.one('#xpto .site-search.query-input').simulate('keydown', {keyCode:13});
+
+        waitsFor(function () {
+            return Y.one('#xpto .site-search.results').all('a').size() > 0;
+        }, 1000, "Test must yield results");
+
+    });
+
+    it("test searching from widget with no results", function () {
+        Y.Node.create('<div id="xpto1"></div>').appendTo('body');
+
+        var widget = new SiteSearch.Widget(),
+            results;
+        widget.render('#xpto1');
+
+        Y.one('#xpto1 .site-search.query-input').set('value', 'pot');
+        Y.one('#xpto1 .site-search.query-input').simulate('keydown', {keyCode:13});
+
+        waitsFor(function () {
+            return Y.one('#xpto1 .site-search.results').all('a').size() === 0;
+        }, 1000, "Pot must not yield results");
+    });
+
 });
